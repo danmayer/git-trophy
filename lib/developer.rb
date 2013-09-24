b94e9ba4fbfc45087f4b8c6d1340c566bf57f2e8
Christian Doyle <christian.l.doyle@gmail.com>
2013-09-23 22:16:53 -0400

    Added trophies to developer.
    Now stats tracks the trophies on a per developer basis.

--- lib/developer.rb
+++ lib/developer.rb
@@ -1,8 +1,14 @@
 class Developer
-  attr_reader :name, :email
+  attr_reader :name, :email, :trophies
 
   def initialize(hash)
     @name = hash[:name]
     @email = hash[:email]
+    @trophies = []
+  end
+
+  # Awards the developer the given trophy
+  def award(trophy)
+    @trophies << trophy 
   end
 end
